6773dc41b371b20fed07eb3de0609fe6cfdfa194
Bobby St. Pierre <bobbysaint96@gmail.com>
2021-08-18 14:17:29 -0400

    using useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from "./useForm";
+import { Hello } from "./Hello";
 
 const App = () => {
-	const [values, handleChange] = useForm({email: '', password: ''});
-
+	const [values, handleChange] = useForm({
+		email: '',
+		password: '',
+		firstName: 
+	''});
+	const [showHello, setShowHello] = useState(true);
+	
+	// useEffect(() => { // useEffect is called one time on initial render, and every time it's changed
+	// 	console.log('render');
+	// 	return () => {
+	// 		console.log("unmount");
+	// 	}
+	// }, [values.password, values.email]);	// optional dependency array- only changes to the properties listed will trigger useEffect, empty array will call useEffect only on initial render)
+	
 	return (
 		<div>
-			<input
-				name="email"
-				value={values.email}
-				onChange={handleChange}
-			/>
-			<input
-					type="password"
-					name="password"
-					value={values.password}
-					onChange={handleChange}
-				/> 
+			<>
+				<button onClick={() => setShowHello(!showHello)}>toggle</button>
+				{showHello && <Hello />}
+					<input name="email" value={values.email} onChange={handleChange} />
+					<input name="firstName" placeholder="bobby" value={values.firstName} onChange={handleChange} />
+					<input type="password" name="password" value={values.password} onChange={handleChange} />
+			</>
 		</div>
 	);
 };
